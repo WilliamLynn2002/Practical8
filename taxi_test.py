9eeef5417382ab10071dbf7ea07ef24345d5b252
WilliamLynn2002 <lynnmyataung@my.jcu.edu.au>
2020-05-29 12:10:04 +0630

    Test my car.

---
+++ taxi_test.py
@@ -0,0 +1,16 @@
+"""Github Link -"""
+
+from taxi import Taxi
+
+
+def main():
+    my_car = Taxi("Prius 1", 100)
+    my_car.drive(40)
+    print(my_car)
+    my_car.start_fare()
+    my_car.drive(100)
+    print(my_car)
+
+
+if __name__ == '__main__':
+    main()
